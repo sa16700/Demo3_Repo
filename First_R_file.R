b34366ef006ab6af9cf4e099af856466af27f8ef
Saddam <saddamabbasi@yahoo.com>
2021-09-25 08:33:12 +0300

    Update First_R_file.R

--- First_R_file.R
+++ First_R_file.R
@@ -1,3 +1,9 @@
 # This is a R file to check how files are updated in Repository of Github 
 x = 5
 if(x < 6) 5^2 else sqrt(5)
+### # Some Changes
+z = 6
+vec = double()
+for(i in z)
+	vec[i] = rnorm(5)
+##################
